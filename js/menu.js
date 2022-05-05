a6d453b918640c385629ee7e59dc28d22af88497
Rodrigo Pedroso <99103890+R0drigo-Pedroso@users.noreply.github.com>
2022-05-05 10:14:47 -0300

    funções css.
    fazendo a animação css
    js botão menu

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,17 @@
+
+// Configuração do menu mobile
 let botaoMenu = document.querySelector('.icone');
 let menu = document.querySelector('.menu');
 
 botaoMenu.addEventListener('click', function(event) {
-    preventDefault();
+    event.preventDefault();
 
     menu.classList.toggle("menu-aberto");
+
+    if (menu.classList.contains("menu-aberto")) {
+        botaoMenu.innerHTML = "Fechar &times;";
+    } 
+    else { botaoMenu.innerHTML = "Menu &equiv;"; 
+    } 
 });
+// Final Configuração do menu mobile
